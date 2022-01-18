f044e41b714d063468db3ebff89c73fa82b51deb
Lisa <lisa.tagliaferri@gmail.com>
2022-01-18 01:51:52 -0500

    Initialization

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+  // Set custom directories for input, output, includes, and data
+  eleventyConfig.addPassthroughCopy("src/style.css");
+  
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "docs"
+    }
+  };
+};
---
+++ src/blog/welcome.md
@@ -0,0 +1,9 @@
+---
+layout: layout.njk
+title: Welcome
+tags: blog
+---
+
+# Welcome
+
+Welcome to the blog
